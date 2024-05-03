47f0b662c8a1dec440fad8f25ac8010bc61feb90
kitkas0000 <ng.dinhduc.2311@gmail.com>
2024-05-03 11:24:17 +0700

    chore: Update phone number in Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,17 +7,13 @@ const Footer = () => {
       <div className="screen-max-width">
         <div>
           <p className="font-semibold text-gray text-xs">
-            More ways to shop: {""}
-            <span className="underline text-blue"> Find an Apple Store </span>
-            or <span className="underline text-blue">
-              {" "}
-              other retailer{" "}
-            </span>{" "}
-            near you.
+            More ways to shop:{" "}
+            <span className="underline text-blue">Find an Apple Store </span>
+            or <span className="underline text-blue">other retailer</span> near
+            you.
           </p>
-
           <p className="font-semibold text-gray text-xs">
-            Or call 1-800-MY-APPLE.
+            Or call 000800-040-1966
           </p>
         </div>
 
@@ -25,18 +21,17 @@ const Footer = () => {
 
         <div className="flex md:flex-row flex-col md:items-center justify-between">
           <p className="font-semibold text-gray text-xs">
-            Copyright @ 2024 Apple Inc. All rights reserved.
+            Copright @ 2024 Apple Inc. All rights reserved.
           </p>
           <div className="flex">
-            {footerLinks.map((link) => {
+            {footerLinks.map((link, i) => (
               <p key={link} className="font-semibold text-gray text-xs">
-                {link}
-                {""}
+                {link}{" "}
                 {i !== footerLinks.length - 1 && (
                   <span className="mx-2"> | </span>
                 )}
-              </p>;
-            })}
+              </p>
+            ))}
           </div>
         </div>
       </div>
