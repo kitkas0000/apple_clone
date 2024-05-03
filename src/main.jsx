0e46b863106db2d5cbef538afad42d50962ddb35
kitkas0000 <ng.dinhduc.2311@gmail.com>
2024-05-03 09:51:00 +0700

    chore: Update .gitignore and package.json, configure Sentry for error tracking

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+//...
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://ebe611193dfd6ac5ef186dd5bc125c7a@o4507190138241024.ingest.de.sentry.io/4507190140731472",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.metrics.metricsAggregatorIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
